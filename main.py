cfbcb9e3939ed426304c6a0323f16ec68f6c492c
SebastianOpiyo <sebamopale@gmail.com>
2020-09-14 20:43:11 +0300

    Add a requests and beautifulsoup utility code.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""This application is a micro-service based scraper that is intent to run
+both locally and on the cloud.
+- It will be a web-scraper-as-a-service, using common tools in python, container, and cloud ecosystem.
+TOOLS:
+    - requests
+    - urllib
+    - BeautifulSoup
+    - PhantomJS
+    - Selenium
+"""
+
