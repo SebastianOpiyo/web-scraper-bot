42c295e92d154f36c48a4b549a0185d4f9bc493d
SebastianOpiyo <sebamopale@gmail.com>
2020-10-14 12:33:06 +0300

    Add write to csv functionality.

--- src/toll_scraper.py
+++ src/toll_scraper.py
@@ -12,36 +12,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
+from Screenshot import Screenshot_Clipping
+
 
 import requests
 import time
 import psutil
+import csv
 
 """After login we have to do the following:
 1. Select each row and View Details per rows
 2. Scrape the selected View.
 3. Convert data into CSV
-4. Push data to Amazon s3"""
-
-"""
-
-def get_upcoming_events(url):
-    # this is the only line that changes
-    # the rest remain the same as in selenium.
-    # driver = webdriver.PhantomJS(executable_path='/home/intelligentbots/Projects/web-scraper-bot/phantomjs/')
-    # driver.get(url)
-    #
-    # events = driver.find_elements_by_xpath('/html/body/div[2]/div/div[4]/div[3]/div/div/form/div/div[6]/h4[3]/b/b')
-    #
-    # for event in events:
-    #     event_details = dict()
-    #     event_details['name'] = event.find_element_by_xpath('h3[@class="event-title"]/a').text
-    #     event_details['location'] = event.find_element_by_xpath('p/span[@class="event-location"]').text
-    #     event_details['time'] = event.find_element_by_xpath('p/time').text
-    #     print(event_details)
-    # # When done, we don't want to populate our display with lots of open web
-    # # browser tabs.
-    # driver.close()
+4. Push data to Amazon s3
 """
 
 
@@ -54,8 +37,10 @@ class ScrapeTolls(TollWebsiteAccess):
         """Check all the checkboxes in the toll list so as
         to get the dynamically generated data using the web driver."""
         # main_run()
+        # currently doesn't fully work as expected.
         try:
-            WebDriverWait(self.driver, 120).until(EC.element_to_be_clickable((By.ID, 'checkAll')))
+            time.sleep(120)
+            # WebDriverWait(self.driver, 120).until(EC.element_to_be_clickable((By.ID, 'checkAll')))
             # check_all_boxes = self.driver.find_element_by_xpath('/html/body/div[2]/div/div[4]/div[3]/div/div/form/'
             #                                                     'div/div[11]/table/thead/tr/th[1]/div/ins"]')
             check_box = self.driver.find_element_by_id('checkAll')
@@ -63,10 +48,10 @@ class ScrapeTolls(TollWebsiteAccess):
             actions.move_to_element(check_box).perform()
             self.driver.execute_script("arguments[0].click();", check_box)
             time.sleep(5)
-            self.driver.get_screenshot_as_file('check-page.png')
+            # self.take_screen_shot('check-page.png')
             print(f'Selected all checkboxes')
         except Exception as e:
-            print(f'Could not check the box because of: {e}')
+            raise e
 
     def scrape_title_info(self):
         load_page = self.driver.find_element_by_xpath('html/body')
@@ -90,25 +75,56 @@ class ScrapeTolls(TollWebsiteAccess):
             item = row.find_element_by_xpath('//*[@id="violationTable"]/tbody/tr').text
             toll_list.append(item)
             # when done, write it into a csv file.
+            self.write_toll_to_csv(toll_list)
+
+    @staticmethod
+    def write_toll_to_csv(toll_list=None, toll_acc=None):
+        if toll_list is not list:
+            print(f'{toll_list} needs to be a list')
+            raise Exception
+        else:
+            pass
+
+        if toll_acc is not dict:
+            print(f'{toll_acc} needs to be a dict')
+            raise Exception
+        else:
+            pass
+
+        with open('tolls.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # write to csv the account details
+            if toll_acc:
+                for item in toll_acc:
+                    csv_writer.writerow(toll_acc[item])
+            # write to csv the tolls scrapes
+            if toll_list:
+                for item in toll_list:
+                    csv_writer.writerow(item)
 
     def execute_view_detail(self):
         """Calls the javascript View Details function on the table so as to create
         the dynamic content table."""
         pass
 
-    def take_screen_shot(self, filename):
-        file_path = f'./screenshots/{filename}'
-        body = self.driver.find_element_by_tag_name('body')
-        body.screenshot(file_path)
+    def take_screen_shot(self, filename: str):
+        # Takes the screenshot of what the robot has achieved anonymously.
+        # that way we can be able to tell whether the robot is doing what we have programmed it to.
+        obj = Screenshot_Clipping.Screenshot()
+        img_url = obj.full_Screenshot(self.driver, save_path=r'./screenshots', image_name=f'{filename}')
+        print(img_url)
 
     def move_to_next_page(self):
+        # since next page is an img link we need to find a way to substitute the
+        # number a[n] with the correct page number.
+        # by default n=1, so we need to increment it by 1 after every page move.
+        # on page 1 n=2 and on page 2 n=3 etc.
         try:
             load_page = self.driver.find_element_by_xpath('html/body')
             next_icon = load_page.find_element_by_xpath('//*[@id="sb-site"]/div[3]/div/div/form/div/div[11]/'
                                                         'div[1]/center/a[2]/img')
             self.driver.execute_script("arguments[0].click();", next_icon)
-            time.sleep(15)
-            self.driver.get_screenshot_as_file('next-page-img.png')
+            time.sleep(6)
             print(f'Moved to the next page!')
         except Exception as e:
             print(f'Could not move to the next page because of the Error: {e}')
