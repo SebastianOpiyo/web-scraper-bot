b3cce30d1d63586dd9675e26c7debddea7351aa8
SebastianOpiyo <sebamopale@gmail.com>
2020-10-28 14:28:51 +0300

    Modified csv writer function.

--- src/base.py
+++ src/base.py
@@ -4,11 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class BasePage:
-    """Driver class inherited by all the other classes."""
+    """Driver class inherited by all the other classes.
+    - It does initiate the chrome driver, chrome browser being the option.
+    - It also closes the browser if needed."""
 
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('headless')
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-        # self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.maximize_window()
+
+    def exit_driver(self):
+        self.driver.quit()
