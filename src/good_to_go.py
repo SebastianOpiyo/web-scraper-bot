20c4d544464ff9586f550ff84efc9739a4079d13
SebastianOpiyo <sebamopale@gmail.com>
2020-11-17 14:27:59 +0300

    Add scraping logic to The_Toll_Roads acc.

--- src/good_to_go.py
+++ src/good_to_go.py
@@ -7,6 +7,7 @@
 
 
 from src.login_script import TollWebsiteAccess, BotExceptionHandler
+from selenium.webdriver.support.ui import Select
 from src.toll_scraper import ScrapeTolls
 import time
 
@@ -39,6 +40,7 @@ class GoodToGo(TollWebsiteAccess):
             login_button = self.driver.find_element_by_xpath('//*[@id="managerYourAccount"]/div[2]/'
                                                              'div/div/div/div/div/div[5]/div')
             self.driver.execute_script("arguments[0].click();", login_button)
+            time.sleep(3)
             ScrapeTolls.take_screen_shot(self, 'login_check.png')
         except BotExceptionHandler:
             print("Timeout exception or Wrong Credentials!")
@@ -47,8 +49,30 @@ class GoodToGo(TollWebsiteAccess):
         pass
 
     def scrape_tolls(self):
-        pass
+        # Select Account history (located at the top)
+        account_history = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[1]/div/div/div'
+                                                            '/div/div[2]/div/div/div/div/div/div[2]/div/div/div[2]')
+        self.driver.execute_script("arguments[0].click();", account_history)
+        # transaction_button = self.driver.find_element_by_xpath('//*[@id="myAccountHeader"]/div/div[2]'
+        #                                                        '/div/div/div/div/div/div[2]/div/div/div[2]/div')
+        # Enter start & end date
+        start_date_value = input('Enter Start Date: ')
+        end_date_value = input('Enter End Date: ')
+        start_date = self.driver.find_elements_by_class_name('v-textfield v-datefield-textfield')
+        end_date = self.driver.find_element_by_xpath('')
+        self.driver.execute_script("arguments[0].click();", start_date)
+        start_date.clear()
+        start_date.send_keys(start_date_value)
+        self.driver.execute_script("arguments[0].click();", end_date)
+        end_date.clear()
+        end_date.send_keys(end_date_value)
+
+        # Select toll under transaction type, click search
+        select = Select(self.driver.find_element_by_id('gwt-uid-102'))
+        select.select_by_visible_text('Toll')
+        # Download CSV
+        GoodToGo.download_tolls(self)
 
     def download_tolls(self):
-        pass
-
+        download_link = self.driver.find_element_by_link_text('(download as CSV)')
+        self.driver.execute_script("arguments[0].click();", download_link)
--- src/the_toll_roads.py
+++ src/the_toll_roads.py
@@ -47,7 +47,25 @@ class TollRoads(TollWebsiteAccess):
         pass
 
     def scrape_tolls(self):
-        pass
+        # Click statements & Activity
+        statement_button = self.driver.find_element_by_link_text('Statements & Activity')
+        self.driver.execute_script("arguments[0].click();", statement_button)
+        # Click find a Transaction
+        find_transaction = self.driver.find_element_by_link_text('Find a Transaction')
+        self.driver.execute_script("arguments[0].click();", find_transaction)
+        # Enter start & end dates & click view transactions
+        start_date_value = input('Enter Start Date: ')
+        end_date_value = input('Enter End Date: ')
+        start_date = self.driver.find_elements_by_id('start-date')
+        end_date = self.driver.find_element_by_id('end-date')
+        self.driver.execute_script("arguments[0].click();", start_date)
+        start_date.clear()
+        start_date.send_keys(start_date_value)
+        self.driver.execute_script("arguments[0].click();", end_date)
+        end_date.clear()
+        end_date.send_keys(end_date_value)
+        view_transaction = self.driver.find_element_by_id('btn-view')
+        self.driver.execute_script("arguments[0].click();", view_transaction)
 
     def download_tolls(self):
         pass
