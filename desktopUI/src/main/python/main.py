f80678b02f4412e37bf788a158dccbd27e97a9a2
SebastianOpiyo <sebamopale@gmail.com>
2020-11-25 07:41:29 +0300

    Convert the desktop ui to python code.

--- desktopUI/src/main/python/main.py
+++ desktopUI/src/main/python/main.py
@@ -6,63 +6,22 @@
 # -*- coding: utf-8 -*-
 
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from desktopUI.uidesign.scraperobotui import Ui_TollScraperRobot
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 import sys
 
 
-class Window(QMainWindow):
+class MainWindow(QMainWindow, Ui_TollScraperRobot):
 
     """Main Window."""
-
-    def __init__(self, *args, **kwargs):
-        """Initializer."""
-        super(QMainWindow, self).__init__(*args, **kwargs)
-
-        self.setWindowTitle('Toll Scraper Robot')
-
-        self.setFixedSize(600, 350)
-        # set the central widget
-        self._centralWidget = QWidget(self)
-        # Set menu
-        self._createMenu()
-        self._createToolBar()
-        self._createStatusBar()
-        self._createComboBox()
-        self._createpushbuttons()
-
-    def _createMenu(self):
-        self.menu = self.menuBar().addMenu("&Menu")
-        self.menu.addAction('&Exit', self.close)
-
-    def _createToolBar(self):
-        tools = QToolBar()
-        self.addToolBar(tools)
-        tools.addAction('Exit', self.close)
-
-    def _createComboBox(self):
-        combo = QComboBox()
-        combo.addItems(["SUNPASS(FL)", "GOODTOGO(WA)", "EZPASS(PA)", "PIKEPASS(OK)", "EZPASS(OH)", "EZPASS(NJ)",
-                             "QUICKPASS(NC)", "RIVERLINK(KY)", "IPASS(IL)", "EXPRESS(CO)", "THETOLLROADS(CA)",
-                             "FASTTRACK(CA)", "TXTAG", "HCTRA(TX)"])
-        self.setCentralWidget(combo)
-
-    def _createDateEdit(self):
-        pass
-
-    def _createpushbuttons(self):
-        pass
-
-    def _createStatusBar(self):
-        status = QStatusBar()
-        status.showMessage("Ready!")
-        self.setStatusBar(status)
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = Window()
+    win = MainWindow()
     win.show()
     sys.exit(app.exec_())
